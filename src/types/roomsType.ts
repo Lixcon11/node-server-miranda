d7ae774399fa395474d914a1e3372e2cbb284518
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-07-16 19:19:01 +0200

    Files realocated

---
+++ src/types/roomsType.ts
@@ -0,0 +1,70 @@
+type IdObject = {
+    id: number
+}
+
+type Amenitie = 
+    | "AC" 
+    | "Breakfast" 
+    | "Cleaning" 
+    | "Grocery" 
+    | "Shop Near"
+    | "Wifi"
+    | "Kitchen"
+    | "Shower"
+    | "Single Bed"
+    | "Towels";
+
+type RoomType = 
+    | "Single Bed"
+    | "Double Bed"
+    | "Double Superior"
+    | "Suite";
+
+
+
+type BookingState = IdObject & {
+    name: string;
+    orderDate: string;
+    checkInDate: string;
+    checkOutDate: string;
+    specialRequest: string;
+    room: RoomState;
+    status: "Check In" | "Check Out" | "In Progress";
+}
+
+
+type RoomState = IdObject & {
+    roomNumber: string;
+    description: string;
+    photos: string[];
+    roomType: RoomType;
+    amenities: Amenitie[];
+    price: number;
+    discount: number;
+    status: "Available" | "Booked";
+}
+
+type ContactState = IdObject & {
+    name: string;
+    date: string;
+    email: string;
+    phone: string;
+    subject: string;
+    comment: string;
+    status: "Published" | "Archived"
+}
+
+type UserState = IdObject & {
+    name: string;
+    photo: string;
+    email: string;
+    phone: string;
+    date: string;
+    job: string;
+    description: string;
+    status: "Active" | "Inactive";
+    password: string;
+}
+
+
+export { UserState, ContactState, BookingState, RoomState}
