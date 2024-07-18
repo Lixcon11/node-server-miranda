092f35971fef39d89d50011e2d30a5b0801919b1
Lixcon11 <106622009+Lixcon11@users.noreply.github.com>
2024-07-19 01:20:51 +0200

    Tests done

---
+++ __tests__/login.test.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { app } from '../src/app';
+
+describe('Hotel API', () => {
+  let token: string;
+
+  beforeAll(async () => {
+      const response = await request(app).post('/login').send({ email: 'hi@mark.com', password: 'youaretearingapartlisa' });
+      
+      expect(response.status).toBe(200);
+      token = response.body.token;
+      expect(token).toBeDefined();
+  });
+
+  it('Should return 401 for unauthenticated request to /rooms', async () => {
+      const response = await request(app).get('/rooms');
+      expect(response.status).toBe(401);
+  });
+
+  it('Should return 200 and list of rooms for authenticated request to /rooms', async () => {
+      const response = await request(app).get('/rooms').set('Authorization', `Bearer ${token}`);
+      
+      expect(response.status).toBe(200);
+      expect(response.body.rooms).toBeDefined();
+  });
+
+  it('Should return 200 and room details for authenticated request to /rooms/:id', async () => {
+      const roomId = 1;
+      const response = await request(app).get(`/rooms/${roomId}`).set('Authorization', `Bearer ${token}`);
+      
+      expect(response.status).toBe(200);
+      expect(response.body.room).toBeDefined();
+      expect(response.body.room.id).toBe(roomId);
+  });
+});
